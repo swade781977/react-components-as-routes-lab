62fc19f166b2337809a0d33298b65d95b29c9a25
swade781977 <swade781977@yahoo.com>
2021-04-03 23:58:02 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,16 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>{actor.name}
+          <ul>
+            {actor.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>  
+        </div>
+      ))}
     </div>
   );
 };
